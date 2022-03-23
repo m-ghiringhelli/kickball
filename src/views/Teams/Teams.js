9f8f826bd7eeba5200d0b739ab4dc1b847a25493
m-ghiringhelli <mghiring@gmail.com>
2022-03-23 15:44:18 -0700

    fetches teams

--- src/views/Teams/Teams.js
+++ src/views/Teams/Teams.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getTeams } from '../../services/teams';
 
 export default function Teams() {
+  const [teams, setTeams] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getTeams();
+        setTeams(data);
+      } catch (e) {
+        setErrorMessage('There was an issue loading the teams');
+      }
+    };
+    fetchData();
+  }, []);
+  
   return (
     <div>Teams</div>
   );
