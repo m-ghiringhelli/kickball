f94ccfc6c014db755039e9db429d211c87863e67
m-ghiringhelli <mghiring@gmail.com>
2022-03-23 17:17:23 -0700

    get players for each selected team

--- src/services/players.js
+++ src/services/players.js
@@ -1,6 +1,11 @@
 import { client } from './client.js';
 
-export async function getPlayers() {
+export async function getPlayers(team) {
+  //pass it the number of the id of the team
+  if (team) {
+    const request = await client.from('players').select('*').match({ team_id: team });
+    return request;
+  }
   const request = await client.from('players').select('*');
   return request;
 } 
--- src/views/Teams/TeamDetail.js
+++ src/views/Teams/TeamDetail.js
@@ -1,16 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { getTeamById } from '../../services/teams';
+import { getPlayers } from '../../services/players';
 import { useParams } from 'react-router-dom';
+import Team from '../../components/Team/Team';
 
 export default function TeamDetail() {
   const id = useParams().id;
   const [team, setTeam] = useState(null);
+  const [teamPlayers, setTeamPlayers] = useState([]);
 
   useEffect(() => {
     getTeamById(id).then(({ data }) => setTeam(data));
+    getPlayers(id).then(({ data }) => setTeamPlayers(data));
   }, [id]);
-  console.log(team);
+
   return (
-    <div>TeamDetail</div>
+    <div>
+      <Team />
+    </div>
   );
 }
