2bc60f4b413e959e0d8891da732a2bc5a65c8e31
m-ghiringhelli <mghiring@gmail.com>
2022-03-24 12:44:11 -0700

    added image to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import './Header.css';
 
 export default function Header() {
   return (
-    <header>
+    <header style={{
+      backgroundImage: `url(${process.env.PUBLIC_URL + '/images/green-hill.jpg'})`
+    }}>
       <ul>
         <li>
           <NavLink exact to='/' className='link'>
