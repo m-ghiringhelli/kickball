62af5befce63025980c56d0b3ebe37c6671bd165
m-ghiringhelli <mghiring@gmail.com>
2022-03-24 17:46:08 -0700

    again

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('contains home', () => {
   render(<App />);
-  const linkElement = screen.getByText(/home/i);
+  const linkElement = screen.getAllByText(/home/i);
   expect(linkElement).toBeInTheDocument();
 });
