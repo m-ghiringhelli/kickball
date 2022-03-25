b73fc48b3b4153371f311d9993f474024fcc7740
m-ghiringhelli <mghiring@gmail.com>
2022-03-24 17:40:28 -0700

    test....

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/kickball/i);
+  const linkElement = screen.getByText(/home/i);
   expect(linkElement).toBeInTheDocument();
 });
